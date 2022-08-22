ccf3693e44512ce0b8f89fb72e3c7e2f2183b8aa
SBellUT <111801926+SBellUT@users.noreply.github.com>
2022-08-22 14:20:37 -0600

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,10 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showIcon(IconNames.Heart)
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showIcon(IconNames.Pitchfork)
+})
+basic.showIcon(IconNames.Heart)
+basic.forever(function () {
+	
+})
